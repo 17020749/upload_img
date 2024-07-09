0d82466fa866b3af663148530b667a08d1906181
Nguyen Trung Hieu <71979577+17020749@users.noreply.github.com>
2024-07-09 12:04:27 +0700

    update trang tru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,85 +1,130 @@
 <x-ui-layout>
     <div id="home">
-      <div class="relative w-full h-[320px]" id="home">
+      <!-- <div class="relative w-full h-[320px]" id="home">
           <div class="absolute inset-0 opacity-70">
               <img src="/images/background.jpg" alt="Background Image" class="object-cover object-center w-full h-full" />
 
           </div>
           <div class="absolute inset-9 flex flex-col md:flex-row items-center justify-between">
               <div class="md:w-1/2 mb-4 md:mb-0">
-                  <!-- <h1 class="text-grey-700 font-medium text-4xl md:text-5xl leading-tight mb-2">Đồ chơi xe máy</h1>
-                  <p class="font-regular text-xl mb-8 mt-4">Cách làm đẹp cho xe của bạn</p> -->
+                  <h1 class="text-grey-700 font-medium text-4xl md:text-5xl leading-tight mb-2">Đồ chơi xe máy</h1>
+                  <p class="font-regular text-xl mb-8 mt-4">Cách làm đẹp cho xe của bạn</p>
                   <a href="#contactUs"
                       class="px-6 py-3 bg-[#c8a876] text-white font-medium rounded-full hover:bg-[#c09858]  transition duration-200">Liên hệ chúng tôi</a>
               </div>
           </div>
-      </div>
+      </div> -->
           <!--HTML CODE-->
       <section  id="services">
-        <div class="md:container md:mx-auto md:px-20">
+        <div class="md:container md:mx-auto lg:px-20 2xl:px-40">
           <div class=" p-1">
-            <h2 class="md:text-3xl font-bold  text-center">Một số sản phẩm nổi bật, bán chạy</h2>
+            <!-- <h2 class="md:text-3xl font-bold  text-center">Một số sản phẩm nổi bật, bán chạy</h2> -->
           </div>
           <swiper-container class="mySwiper" init="false" >
-            <swiper-slide><div class=" rounded-2xl h-84 flex justify-center items-center">
-                          <span class="text-2xl font-semibold text-indigo-600">
-                          <div class="  rounded-lg shadow-md overflow-hidden ">
-                              <img src="/images/introduct1.jpeg" alt="wheat flour grinding"
-                                  class="w-full  object-cover">
-                              <div class="p-1 text-center">
-                                  <h3 class="text-xl font-medium text-gray-800 ">Wheat Flour Grinding</h3>
-                              </div>
-                          </div>
-                          </span>
-                        </div></swiper-slide>
-            <swiper-slide><div class=" rounded-2xl h-84 flex justify-center items-center">
-                          <span class="text-2xl font-semibold text-indigo-600">
-                          <div class="  rounded-lg shadow-md overflow-hidden ">
-                              <img src="/images/introduct2.jpeg" alt="wheat flour grinding"
-                                  class="w-full  object-cover">
-                              <div class="p-1 text-center">
-                                  <h3 class="text-xl font-medium text-gray-800 ">Wheat Flour Grinding</h3>
-                              </div>
-                          </div>
-                          </span>
-                        </div></swiper-slide>
-            <swiper-slide><div class=" rounded-2xl h-84 flex justify-center items-center">
-                          <span class="text-2xl font-semibold text-indigo-600">
-                          <div class="  rounded-lg shadow-md overflow-hidden ">
-                              <img src="/images/introduct16.jpeg" alt="wheat flour grinding"
-                                  class="w-full  object-cover">
-                              <div class="p-1 text-center">
-                                  <h3 class="text-xl font-medium text-gray-800">Wheat Flour Grinding</h3>
-                              </div>
-                          </div>
-                          </span>
-                        </div></swiper-slide>
-            <swiper-slide><div class=" rounded-2xl h-84 flex justify-center items-center">
-                          <span class="text-2xl font-semibold text-indigo-600">
-                          <div class="  rounded-lg shadow-md overflow-hidden ">
-                              <img src="/images/introduct3.jpeg" alt="wheat flour grinding"
-                                  class="w-full  object-cover">
-                              <div class="p-1 text-center">
-                                  <h3 class="text-xl font-medium text-gray-800">Wheat Flour Grinding</h3>
-                              </div>
-                          </div>
-                          </span>
-                        </div></swiper-slide>
-            <swiper-slide><div class=" rounded-2xl h-84 flex justify-center items-center">
-                          <span class="text-2xl font-semibold text-indigo-600">
-                          <div class="  rounded-lg shadow-md overflow-hidden ">
-                              <img src="/images/introduct4.jpeg" alt="wheat flour grinding"
-                                  class="w-full  object-cover">
-                              <div class="p-1 text-center">
-                                  <h3 class="text-xl font-medium text-gray-800">Wheat Flour Grinding</h3>
-                              </div>
-                          </div>
-                          </span>
-                        </div></swiper-slide>
-            <swiper-slide>Slide 6</swiper-slide>
-            <swiper-slide>Slide 7</swiper-slide>
-            <swiper-slide>Slide 8</swiper-slide>
-            <swiper-slide>Slide 9</swiper-slide>
+            <swiper-slide>
+             <div class="max-w-sm overflow-hidden shadow-lg">
+              <img class="w-full object-cover" src="/images/introduct1.jpeg" alt="Sunset in the mountains">
+              <div class="px-1 py-1">
+                <p class="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet 
+                </p>
+              </div>
+              <div class="px-1 pb-1">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              </div>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+             <div class="max-w-sm overflow-hidden shadow-lg">
+              <img class="w-full object-cover" src="/images/introduct2.jpeg" alt="Sunset in the mountains">
+              <div class="px-1 py-1">
+                <p class="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet 
+                </p>
+              </div>
+              <div class="px-1 pb-1">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              </div>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+             <div class="max-w-sm overflow-hidden shadow-lg">
+              <img class="w-full object-cover" src="/images/introduct3.jpeg" alt="Sunset in the mountains">
+              <div class="px-1 py-1">
+                <p class="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet 
+                </p>
+              </div>
+              <div class="px-1 pb-1">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              </div>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+             <div class="max-w-sm overflow-hidden shadow-lg">
+              <img class="w-full object-cover" src="/images/introduct4.jpeg" alt="Sunset in the mountains">
+              <div class="px-1 py-1">
+                <p class="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet 
+                </p>
+              </div>
+              <div class="px-1 pb-1">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              </div>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+             <div class="max-w-sm overflow-hidden shadow-lg">
+              <img class="w-full object-cover" src="/images/introduct5.jpeg" alt="Sunset in the mountains">
+              <div class="px-1 py-1">
+                <p class="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet 
+                </p>
+              </div>
+              <div class="px-1 pb-1">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              </div>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+             <div class="max-w-sm overflow-hidden shadow-lg">
+              <img class="w-full object-cover" src="/images/introduct6.jpeg" alt="Sunset in the mountains">
+              <div class="px-1 py-1">
+                <p class="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet 
+                </p>
+              </div>
+              <div class="px-1 pb-1">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              </div>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+             <div class="max-w-sm overflow-hidden shadow-lg">
+              <img class="w-full object-cover" src="/images/introduct7.jpeg" alt="Sunset in the mountains">
+              <div class="px-1 py-1">
+                <p class="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet 
+                </p>
+              </div>
+              <div class="px-1 pb-1">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              </div>
+              </div>
+            </swiper-slide>
+            <swiper-slide>
+             <div class="max-w-sm overflow-hidden shadow-lg">
+              <img class="w-full object-cover" src="/images/introduct8.jpeg" alt="Sunset in the mountains">
+              <div class="px-1 py-1">
+                <p class="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet 
+                </p>
+              </div>
+              <div class="px-1 pb-1">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              </div>
+              </div>
+            </swiper-slide>
           </swiper-container>
         </div>
       </section>
@@ -93,7 +138,7 @@
   swiper-container {
       width: 100%;
       height: 100%;
-      padding: 20px;
+      padding: 0.5rem;
     }
 
     swiper-slide {
@@ -103,7 +148,6 @@
       display: flex;
       justify-content: center;
       align-items: center;
-      background-color: rgb(252 165 165);
     }
 
     swiper-slide img {
@@ -121,9 +165,7 @@
       transition-timing-function: linear !important;
       position: relative;
       }
-    .swiper-pagination{
-      position:static;
-    }
+    
 </style>
 <script>
   const swiperEl = document.querySelector('swiper-container')
@@ -132,6 +174,10 @@
       slidesPerView: 2,
       spaceBetween: 10,
       breakpoints: {
+          1200: {
+              slidesPerView: 5,
+              spaceBetween: 10,
+          },
           992: {
               slidesPerView: 4,
               spaceBetween: 10,
@@ -156,6 +202,10 @@
       .swiper-pagination-bullet-active {
         color: #fff;
         background: #007aff;
+      }
+      .swiper-pagination{
+      position:static;\
+      margin-top: 4px;
       }
       `],
       pagination: {
